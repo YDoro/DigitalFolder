0bb637c6de0916386bf326483be1058ac6cceb99
Yan Mello <yan_mvd@hotmail.com>
2019-11-25 12:08:36 -0300

    read document

--- app/controllers/file.js
+++ app/controllers/file.js
@@ -1,17 +1,28 @@
 const User = require("../models/user");
 const Document = require("../models/document");
+
+
+
+//=============================================================================
 module.exports.home = async (app, req, res) => {
   const user = await User.findById(req.userId);
   const documents = await Document.find({ user: req.userId });
   if (!user) res.status(400).send("Error,Please try again!");
   res.render("../views/home", { user, documents });
 };
+
+
+//=============================================================================
 module.exports.create = async (app, req, res) => {
   const user = await User.findById(req.userId);
 
   if (!user) res.status(400).send("Error,Please try again!");
   res.render("../views/newDoc");
 };
+
+
+
+//=============================================================================
 module.exports.new = async (app, req, res) => {
   var fs = require("fs");
   var pdfreader = require("pdfreader");
@@ -24,7 +35,7 @@ module.exports.new = async (app, req, res) => {
       Object.keys(rows)
         .sort((y1, y2) => parseFloat(y1) - parseFloat(y2))
         .forEach(y => {
-          content = content + (rows[y] || []).join(" ") + "\n";
+          content = content+(rows[y] || []).join(" ") ;
         });
       if (end) {
         await Document.create({
@@ -46,7 +57,7 @@ module.exports.new = async (app, req, res) => {
               printRows(false);
               rows = {};
             } else if (item.text)
-              (rows[item.y] = rows[item.y] || []).push(item.text);
+              (rows[item.y] = rows[item.y] || []).push(item.text+'\n');
           });
         } else {
 
@@ -73,6 +84,8 @@ module.exports.new = async (app, req, res) => {
 
   if (!user) res.status(400).send("Error,Please try again!");
 };
+
+//=============================================================================
 module.exports.delete = async (app,req,res)=>{
   var doc = await Document.findById(req.body.id);
   const user = await User.findById(req.userId);
@@ -83,4 +96,11 @@ module.exports.delete = async (app,req,res)=>{
 } else{
   res.status(403).send('Permission denied');
 }
+}
+//=============================================================================
+module.exports.view = async(app,req,res)=>{
+  var doc = await Document.findById(req.params.docId).select('+content');
+  const user = await User.findById(req.userId);
+  if(user._id+'' === doc.user+'')  res.render("../views/viewDoc",{doc});
+  else res.status(403).send('Permission denied');
 }
--- app/routes/file.js
+++ app/routes/file.js
@@ -1,21 +1,22 @@
-const authMiddleware = require('../middlewares/auth')
-const multipart = require('connect-multiparty');
+const authMiddleware = require("../middlewares/auth");
+const multipart = require("connect-multiparty");
 var multipartMiddleware = multipart();
 
-module.exports = (app)=>{
-    app.get('/home',authMiddleware,(req,res)=>{
-        app.app.controllers.file.home(app, req, res);
-
-    })
-    app.get('/document/create',authMiddleware,(req,res)=>{
-     
-        app.app.controllers.file.create(app, req, res);
-
-    })
-    app.post('/document/create',authMiddleware,multipartMiddleware,(req,res)=>{
+module.exports = app => {
+  app.get("/home", authMiddleware, (req, res) => {
+    app.app.controllers.file.home(app, req, res);
+  });
+  app.get("/document/create", authMiddleware, (req, res) => {
+    app.app.controllers.file.create(app, req, res);
+  });
+  app.post("/document/create",authMiddleware,multipartMiddleware,(req, res) => {
       app.app.controllers.file.new(app, req, res);
-    })
-    app.post('/document/remove/',authMiddleware,(req,res)=>{
-        app.app.controllers.file.delete(app, req, res);
-      })
-}
+    }
+  );
+  app.post("/document/remove/", authMiddleware, (req, res) => {
+    app.app.controllers.file.delete(app, req, res);
+  });
+  app.get("/document/view/:docId",authMiddleware,(req,res)=>{
+    app.app.controllers.file.view(app, req, res);
+  });
+};
