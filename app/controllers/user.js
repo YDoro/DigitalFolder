7b8a743c2cd61f92a28f3b00ca2101f41f3428ce
Yan Mello <yan_mvd@hotmail.com>
2019-11-25 13:44:19 -0300

    data to false

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -25,11 +25,11 @@ module.exports.login = async (app, req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email }).select("+password");
   if (!user)
-    return res.render("../views/login", { error: "User not found!", data: {} });
+    return res.render("../views/login", { error: "User not found!", data: false });
   if (!(await bcrypt.compare(password, user.password)))
     return res.render("../views/login", {
       error: "invalid password!",
-      data: {}
+      data: false
     });
 
     var token = generateToken({id:user._id})
