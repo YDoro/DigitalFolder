b4e0fab897dd6d8b00ca8c94971537d60d5bd62e
Yan Mello <yan_mvd@hotmail.com>
2019-11-25 14:30:55 -0300

    order by modifiedAt

--- app/controllers/file.js
+++ app/controllers/file.js
@@ -6,7 +6,7 @@ const Document = require("../models/document");
 //=============================================================================
 module.exports.home = async (app, req, res) => {
   const user = await User.findById(req.userId);
-  const documents = await Document.find({ user: req.userId });
+  const documents = await Document.find({ user: req.userId }).sort('-modifiedAt');
   if (!user) res.status(400).send("Error,Please try again!");
   res.render("../views/home", { user, documents });
 };
